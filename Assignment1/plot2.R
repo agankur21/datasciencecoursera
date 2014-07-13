0251c072374b0a90db11fbd0b66d351bffc1aca0
Ankur Aggarwal <ankur.aggarwal@guavus.com>
2014-07-14 02:00:10 +0530

    Added code and image of plot 2

---
+++ Assignment1/plot2.R
@@ -0,0 +1,10 @@
+myData = read.table("household_power_consumption.txt",header = T,sep = ';')
+head(myData)
+date1 <- as.Date("2007-02-01",format = "%Y-%m-%d")
+date2 <- as.Date("2007-02-02",format = "%Y-%m-%d")
+myData <- transform(myData,Date = as.Date(Date,format = "%d/%m/%Y"))
+relevantData <- myData[myData$Date==date1 | myData$Date==date2,]
+relevantData <- transform(relevantData,Time = strptime(paste(Date,Time),format = "%Y-%m-%d %H:%M:%S"))
+plot(relevantData$Time,relevantData$Global_active_power,type = 'l',ylab = "Global Active Power (kilowatts)",xlab = "")
+dev.copy(png,file = 'plot2.png',width = 480,height = 480,units = 'px')
+dev.off()
