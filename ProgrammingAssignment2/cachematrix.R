4e32a2ac0e54725a96747591b9bc0a9b0dff9177
Ankur Aggarwal <ankur.aggarwal@guavus.com>
2014-07-26 15:42:31 +0530

    Commiting getting cached inverse

---
+++ ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,35 @@
+## The first function, makeVector creates a special "vector", which is really a list containing a function to
+##set the value of the vector
+##get the value of the vector
+##set the value of the mean
+##get the value of the mean
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set,get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+## The following function calculates the mean of the special "vector" created with the above function.
+##However, it first checks to see if the inverse has already been calculated. 
+##If so, it gets the inverse from the cache and skips the computation. 
+##Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setinverse function.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinverse(m)
+        m
+}
